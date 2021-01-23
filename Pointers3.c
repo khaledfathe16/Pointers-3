1b57feb3f30820d07cfae1ce23fd3aedf0e70528
Khaled Fathy <77421372+khaledfathe16@users.noreply.github.com>
2021-01-23 05:09:01 +0200

    .

--- Pointers3.c
+++ Pointers3.c
@@ -1,6 +1,6 @@
 
 /****************************************************************************************************************
-This project to scanf the size of array from client and pass it by address to function and then scan the values *
+This project to scan the size of array from client and pass it by address to function and then scan the values *
    of this array form the same user and print back the Maximum number and Minimum number                      *
  ****************************************************************************************************************/
 #include <Stdio.h>
